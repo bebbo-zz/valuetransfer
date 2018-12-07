fa18a8a4632f6256422c4da5f92b2ac110a24252
Martin Brehm <martin.brehm@web.de>
2018-12-07 19:25:21 +0100

    edit product nicer

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { i18n } from './plugins/i18n.js'
-import { vueditor } from './plugins/vueditor'
 
 Vue.config.productionTip = false
 
@@ -12,6 +11,5 @@ new Vue({
   router,
   store,
   i18n,
-  vueditor,
   render: h => h(App)
 }).$mount('#app')
